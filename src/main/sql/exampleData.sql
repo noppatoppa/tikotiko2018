9ea2298a14ac0301b87df1792b4a5b5cadcc949e
Mäntysaari Jaakko <mantyjaska@gmail.com>
2018-04-23 21:02:55 +0300

    Add new example data
    Add divari D2 and nide rows.

--- src/main/sql/exampleData.sql
+++ src/main/sql/exampleData.sql
@@ -6,3 +6,14 @@ VALUES ('9155430674', 'Madeleine Brent', 'Elektran tytär', 'romantiikka', 'roma
 ('No ISBN available', 'Mika Waltari', 'Komisario Palmun erehdys', 'dekkari', 'romaani', 600),
 ('No ISBN available', 'Shelton Gilbert', 'Friikkilän pojat Mexicossa', 'huumori', 'sarjakuva', 700),
 ('9789510396230', 'Dale Carnegien',  'Miten saan ystäviä, menestystä, vaikutusvaltaa', 'opas', 'tietokirja', 800);
+
+INSERT INTO keskusdivari.divari (nimi, osoite, webosoite)
+VALUES ('Galleinn Galle', 'Gallenpolku 1', 'http://www.gallendivari.foo');
+
+INSERT INTO keskusdivari.nide (teos_id, divari_id, sisaanosto_hinta, hinta)
+VALUES (1, 1, 10, 20),
+(2, 1, 15, 20),
+(3, 1, 12, 13),
+(4, 1, 14, 16),
+(5, 1, 9, 10),
+(6, 1, 4, 5);
