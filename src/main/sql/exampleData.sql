84ebbd509bc56eeaac02e9c39679510a7c7fe8eb
Mäntysaari Jaakko <mantyjaska@gmail.com>
2018-04-25 23:30:13 +0300

    Add more example data

--- src/main/sql/exampleData.sql
+++ src/main/sql/exampleData.sql
@@ -15,6 +15,12 @@ VALUES (1, 1, 10, 20),
 (5, 1, 9, 10),
 (6, 1, 4, 5);
 
+INSERT INTO keskusdivari.asiakas (nimi, osoite, puhnro, email, ktunnus, salasana, admin)
+VALUES ('jaska', 'fsdfads',	'fdsfdsa', 'fdfsad', 'jaska', 'foo', false),
+('admin1', 'osote', 'nro', 'email', 'admin1', 'foo', true),
+('admin2', 'osote', 'nro', 'email', 'admin2', 'foo', true)
+;
+
 INSERT INTO keskusdivari.divari (nimi, osoite, webosoite, admin_id)
 VALUES ('Lassen lehti', 'Lassentie 1', 'http://www.lassenlehti.foo', 2);
 
@@ -36,10 +42,10 @@ VALUES (1, 1, 10, 20),
 (3, 1, 12, 13),
 (4, 1, 14, 16),
 (5, 1, 9, 10),
-(6, 1, 4, 5);
+(6, 1, 4, 5),
+(2, 2, 16, 25),
+(3, 2, 12, 16),
+(4, 2, 14, 19),
+(5, 2, 9, 18),
+(6, 2, 4, 15);
 
-INSERT INTO keskusdivari.asiakas (nimi, osoite, puhnro, email, ktunnus, salasana, admin)
-VALUES ('jaska', 'fsdfads',	'fdsfdsa', 'fdfsad', 'jaska', 'foo', false),
-('admin1', 'osote', 'nro', 'email', 'admin1', 'foo', true),
-('admin2', 'osote', 'nro', 'email', 'admin2', 'foo', true)
-;
